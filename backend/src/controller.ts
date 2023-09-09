755d7dd08f69adc76839a74de0620a53f3191f77
Qiiz <aqz1997@gmail.com>
2023-09-10 01:01:38 +0800

    save and update

--- backend/src/controller.ts
+++ backend/src/controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import Connection from "../db.js"
+import { Equipments } from "./model.js";
+import EquipmentRepo from "./repository.js";
+
 
 export default class Controller {
 
@@ -9,6 +12,63 @@ export default class Controller {
             res.status(200).send({
                 message: 'Database connection is successful.'
             });            
+        } catch (err) {
+            res.status(500).send({
+                message: err
+            })
+        }
+    }
+
+    async save(req: Request, res: Response) {
+        const equipmentInstance: Equipments = req.body
+        const existingEquipment = await EquipmentRepo.retrieveSingle({serial_number: equipmentInstance.serial_number}); // will return an array with one item inside.
+        if (existingEquipment.length >0){
+            res.status(500).send("Item already exist in database!")
+            return;
+        }
+        if (!equipmentInstance){ // make sure that there is something in the body
+            res.status(500).send("Cannot be empty!")
+            return;
+        }
+        
+        try {
+            // run ML prediction here
+            await EquipmentRepo.save(equipmentInstance);
+            res.status(200).send({
+                message: "Success! Saved to database."
+            })
+            
+            
+        } catch (err) {
+            res.status(500).send({
+                message: err
+            })
+        }
+    }
+
+    async updateItem(req: Request, res: Response){
+        try {
+            const updatedEquipment = req.body
+            // Get the existing data
+            const existingEquipment = await EquipmentRepo.retrieveSingle({serial_number: updatedEquipment.serial_number}); // will return an array with one item inside.
+            // existing equipment does not exist
+            if (existingEquipment.length == 0){
+                res.status(400).send({
+                    message: "Equipment not found in database!"
+                })
+                return ;
+            }
+
+            if (!updatedEquipment){ // make sure that there is something in the body
+                res.status(500).send("Cannot be empty!")
+                return;
+            }
+            // run ML prediction here
+            await EquipmentRepo.updateItem(existingEquipment[0], updatedEquipment)
+            res.status(200).send({
+                message: "Success! Saved to database."
+            });    
+
         } catch (err) {
             res.status(500).send({
                 message: err
--- backend/src/repository.ts
+++ backend/src/repository.ts
@@ -1,5 +1,5 @@
+import DBconnection from '../db.js'
 import { Equipments } from './model.js'
-
 
 // do the api end point for medical items and 
 // and also statistics
@@ -29,16 +29,59 @@ interface IEquipmentRepo {
 
 // data layer access for our database 
 class EquipmentRepo implements IEquipmentRepo {
-    save(equipment: Equipments): Promise<void> {
-        throw new Error('Method not implemented.');
+    async save(equipment: Equipments): Promise<void>{
+        const connection = await DBconnection();
+        try {
+            const insertQuery = `INSERT INTO db.Equipments (serial_number, state, equip_name, category, cost, status, functionality, under_warrenty, installation_date, cluster) 
+                                VALUES ('${equipment.serial_number}', 
+                                        '${equipment.state}',
+                                        '${equipment.equip_name}', 
+                                        '${equipment.category}', 
+                                        '${equipment.cost}', 
+                                        '${equipment.status}',
+                                        '${equipment.functionality}',
+                                        '${equipment.under_warrenty}',
+                                        '${equipment.installation_date}',
+                                        '${equipment.cluster}')`;
+    
+            // Execute the insert query
+            const [result] = await connection.execute(insertQuery);
+        } catch (err) {
+            throw err; // Propagate the error to the caller so that they can catch the error
+        } finally{
+            connection.end(); // Close the DB connection once the operation is completed
+        }
     }
+
+    async updateItem(oldEquipment: Equipments, newEquipment: Equipments): Promise<void> {
+        const connection = await DBconnection();
+        const query = `UPDATE db.Equipments
+                        SET 
+                        category = '${newEquipment.category}',
+                        equip_name = '${newEquipment.equip_name}', 
+                        state = '${newEquipment.state}', 
+                        status = '${newEquipment.status}',
+                        cost = '${newEquipment.cost}',
+                        functionality = '${newEquipment.functionality}',
+                        under_warrenty = '${newEquipment.under_warrenty}',
+                        installation_date = '${newEquipment.installation_date}'
+                        cluster = '${newEquipment.cluster}',
+                        WHERE serial_number = '${oldEquipment.serial_number}';`
+
+        try {
+            await connection.execute(query);
+        } catch (err) {
+            console.log(err)
+            throw err; 
+        } finally {
+            connection.end();
+        }
+    }
+
     retrieveAll(searchParams: { serial_number?: string | undefined; }): Promise<Equipments[]> {
         throw new Error('Method not implemented.');
     }
     retrieveSingle(searchParams: { serial_number?: string | undefined; }): Promise<Equipments[]> {
-        throw new Error('Method not implemented.');
-    }
-    updateItem(oldEquipment: Equipments, newEquipment: Equipments): Promise<void> {
         throw new Error('Method not implemented.');
     }
     updateItemStatus(serial_number: String): Promise<void> {
