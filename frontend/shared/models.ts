39e81d9b3220903bd2e3d59bd2a3cb4e91b44446
Aliciaho <60432763+Aliciaho@users.noreply.github.com>
2023-09-10 17:51:48 +0800

    feat: add models

---
+++ frontend/shared/models.ts
@@ -0,0 +1,36 @@
+export interface MedicalItem {
+    serial_number: string; // Primary key
+    equip_name: string;
+    state: string;
+    dateOfInstallation: string;
+    status: string;
+    functionality: string;
+    category: string;
+    cost: number;
+    under_warrenty: boolean;
+}
+
+export interface Activity {
+    serial_number: string; // Primary key
+    state: string;
+    equip_name: string;
+    'Borrow Date': string;
+    'Return Date': string;
+    status: string;
+}
+
+export interface Forecast {
+    category: string; // Primary key
+    quantity: string;
+    in_use: string;
+    available: string;
+    unhealthy: string;
+    forecast: string;
+}
+
+export interface Statistics {
+    totalUnhealthy: string;
+    healtyPercentage: string;
+    totalUnAvailable: string;
+    totalAvailable: string;
+}
